054197eb1ce29ef713f0875d7eb34e0c2564e37f
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2024-06-20 16:26:18 +0800

    Update checkout.py

--- examples/checkout.py
+++ examples/checkout.py
@@ -1,24 +1,25 @@
-import git
-import os
-from io import RawIOBase
-
 localrep = "/home"
+
+from maix import camera, display, image
+
+image.load_freetype("/root/preset/fonts/simhei.ttf")
+hello_img = image.new(size=(320, 240), color=(0, 0, 0), mode="RGB")
+hello_img.draw_string(10, 115, 'Checkout', scale=1.0, color=(255, 255, 255), thickness=1)
+display.show(hello_img)
+
+import os, git
+
+if os.path.exists(localrep + "/cclb_launch"):
+    os.remove(localrep + "/cclb_launch")
+
+from io import RawIOBase
 
 
 class NewStream(RawIOBase):
-    """Fallback if stdout or stderr are unavailable, does nothing."""
-
-    def read(self, size=-1):
-        return None
-
-    def readall(self):
-        return None
-
-    def readinto(self, b):
-        return None
-
     def write(self, b):
-        print(b)
+        hello_img = image.new(size=(320, 240), color=(0, 0, 0), mode="RGB")
+        hello_img.draw_string(10, 115, str(b), scale=1.0, color=(255, 255, 255), thickness=1)
+        display.show(hello_img)
         return None
 
 
@@ -30,8 +31,10 @@ if git.activeBranch(localrep) != b'master':
     git.checkout(localrep, 'master', steam)
 else:
     git.checkout(localrep, 'en', steam)
-if os.path.exists("/root/CocoPi.py"):
-    os.remove("/root/CocoPi.py")
+hello_img = image.new(size=(320, 240), color=(0, 0, 0), mode="RGB")
+hello_img.draw_string(10, 115, 'Reconstruct directory', scale=1.0, color=(255, 255, 255), thickness=1)
+display.show(hello_img)
 os.system('chmod -R 777 /home')
-os.system('reboot')
+os.system('rsync -r --checksum /home/backup/ /root')
+os.system("ps | grep main.py | grep -v grep | awk '{print $1}' | xargs kill")
 git.ro()
