605d13050b509c18371492e301db92a06ded818e
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2024-06-20 16:26:34 +0800

    Update pull.py

--- examples/pull.py
+++ examples/pull.py
@@ -1,34 +1,47 @@
-import git
-import os
-from io import RawIOBase
-
 localrep = "/home"
+
+from maix import camera, display, image  # 引入python模块包
+
+image.load_freetype("/root/preset/fonts/simhei.ttf")
+hello_img = image.new(size=(320, 240), color=(0, 0, 0), mode="RGB")
+hello_img.draw_string(10, 115, 'Pull', scale=1.0, color=(255, 255, 255), thickness=1)
+display.show(hello_img)
+
+import os, git, time
+
+if not git.isOnline(localrep):
+    hello_img = image.new(size=(320, 240), color=(0, 0, 0), mode="RGB")
+    hello_img.draw_string(10, 115, 'Network connection error', scale=1.0, color=(255, 255, 255), thickness=1)
+    display.show(hello_img)
+    time.sleep(2)
+    exit()
+if os.path.exists(localrep + "/cclb_launch"):
+    os.remove(localrep + "/cclb_launch")
+
+from io import RawIOBase
 
 
 class NewStream(RawIOBase):
-    """Fallback if stdout or stderr are unavailable, does nothing."""
-
-    def read(self, size=-1):
-        return None
-
-    def readall(self):
-        return None
-
-    def readinto(self, b):
-        return None
-
     def write(self, b):
-        print(b)
+        hello_img = image.new(size=(320, 240), color=(0, 0, 0), mode="RGB")
+        hello_img.draw_string(10, 115, str(b), scale=1.0, color=(255, 255, 255), thickness=1)
+        display.show(hello_img)
         return None
 
 
 git.rw()
 steam = NewStream()
-if os.path.exists(localrep + "/cclb_launch"):
-    os.remove(localrep + "/cclb_launch")
+
 git.pull(localrep, steam)
-if os.path.exists("/root/CocoPi.py"):
-    os.remove("/root/CocoPi.py")
+hello_img = image.new(size=(320, 240), color=(0, 0, 0), mode="RGB")
+commit, Author, Date, message = git.__log__(localrep)
+hello_img.draw_string(10, 50, commit, scale=1.0, color=(255, 255, 255), thickness=1)
+hello_img.draw_string(10, 100, Author, scale=1.0, color=(255, 255, 255), thickness=1)
+hello_img.draw_string(10, 150, Date, scale=1.0, color=(255, 255, 255), thickness=1)
+hello_img.draw_string(10, 200, message, scale=1.0, color=(255, 255, 255), thickness=1)
+display.show(hello_img)
+
 os.system('chmod -R 777 /home')
-os.system('reboot')
+os.system('rsync -r --checksum /home/backup/ /root')
+os.system("ps | grep main.py | grep -v grep | awk '{print $1}' | xargs kill")
 git.ro()
